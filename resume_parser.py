b84039f8914a692c40c9819891ef90932c08f42f
Arpita120604 <142209037+Arpita120604@users.noreply.github.com>
2026-01-25 22:14:09 +0530

    Update resume_parser.py

--- resume_parser.py
+++ resume_parser.py
@@ -1,13 +1,10 @@
 import PyPDF2
 
 def extract_text_from_pdf(file):
-    pdf_reader = PyPDF2.PdfReader(file)
     text = ""
+    pdf_reader = PyPDF2.PdfReader(file)
 
     for page in pdf_reader.pages:
         text += page.extract_text()
 
     return text
-
-Fix indentation error in resume_paper.py
-
