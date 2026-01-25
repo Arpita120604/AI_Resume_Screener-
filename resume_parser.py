d8e00855b126f4ef46943a0047c079da6233e734
Arpita120604 <142209037+Arpita120604@users.noreply.github.com>
2026-01-25 22:07:52 +0530

    Update resume_parser.py

--- resume_parser.py
+++ resume_parser.py
@@ -1,8 +1,10 @@
 import PyPDF2
 
-def extract_text_from_pdf(uploaded_file):
-    reader = PyPDF2.PdfReader(uploaded_file)
+def extract_text_from_pdf(file):
+    pdf_reader = PyPDF2.PdfReader(file)
     text = ""
-    for page in reader.pages:
+
+    for page in pdf_reader.pages:
         text += page.extract_text()
+
     return text
